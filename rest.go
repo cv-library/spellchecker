86595144791e4a353775bc9416ad203506d675b9
Tim Retout <t.retout@cv-library.co.uk>
2015-05-22 17:16:51 +0100

    Add Access-Control-Allow-Origin header

--- rest.go
+++ rest.go
@@ -95,6 +95,7 @@ func legacyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 
 	w.Write(b)
